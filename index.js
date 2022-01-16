12d3160ec336590ee514bed33d182b93bc14df42
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-01-16 15:38:09 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+});
+
+const target = document.querySelector('#js-target');
+
+scroll.scrollTo(target);
