1c93217699ace350dd7f01b53bad1481b69aaa13
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-04-22 14:23:35 +0200

    Version 1.3 - smarthphone and tablet smooth=true

--- index.js
+++ index.js
@@ -78,6 +78,12 @@ window.addEventListener('load', () => {
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
+  smartphone: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
 });
 
 // SCROLL TO A SPECIFIC POINT
