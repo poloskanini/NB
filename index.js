42d145c978e349b9afbf69f4844dda83be680ddf
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-05-11 20:02:09 +0200

    Version 1.5.1-mobiles OK

--- index.js
+++ index.js
@@ -143,3 +143,14 @@ headerDownObserver.observe(menu);
 // window.addEventListener("scroll", function() {
 //   overlay.style.backgroundPosition = +window.pageYOffset+"px";
 // });
+
+//CURSOR CUSTOMIZED
+const cursor = document.querySelector('.cursor-inner');
+const cursor2 = document.querySelector('.cursor-outer');
+document.addEventListener("mousemove", e => {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+})
