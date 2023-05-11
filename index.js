f935187d74880572075631f279a0255e271a2b75
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-05-11 09:35:38 +0200

    Ajout d'un loader

--- index.js
+++ index.js
@@ -50,13 +50,7 @@ const loader = document.querySelector('.loader');
 
 window.addEventListener('load', () => {
 
-    if(sessionStorage.getItem('premierChargement') === null){
-        loader.classList.add('fondu-out');
-    } else {
-        loader.style.display = "none"; 
-    }
-  
-    sessionStorage.setItem('premierChargement', 'done');
+    loader.classList.add('loaded');
 })
 
 //& LOCOMOTIVE
